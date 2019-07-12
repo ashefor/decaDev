9e161448191e8d22303d0ad92eab42779f7f78f5
Michael Ashefor <thatsureboy@gmail.com>
2019-07-12 04:20:31 +0100

    Remove default google form on success redirect

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -2,12 +2,28 @@ window.addEventListener('load', function () {
     document.getElementById('loading').style.display = "none"
     let menu = document.getElementById('openmenu');
     let othernav = document.getElementById('linksha')
-    menu.addEventListener('click', function(){
-        // alert("message")
-        if(othernav.style.display === 'block'){
+    menu.addEventListener('click', function () {
+        if (othernav.style.display === 'block') {
             othernav.style.display = 'none'
-        }else{
+        } else {
             othernav.style.display = 'block'
         }
+    })
+    let submitted = false;
+    let submitting = document.getElementById('contactform')
+    submitting.addEventListener('submit', function () {
+        submitted = true;
+        let hiddenframe = document.getElementById('hidden_iframe')
+        hiddenframe.addEventListener('load', function () {
+            if (submitted) {
+                document.getElementById('msg_success').style.display = 'block'
+                document.getElementById('msg_success').classList.add('fadeIn')
+                document.getElementById('contactform').reset()
+            }
+            setTimeout(function(){
+                document.getElementById('msg_success').classList.add('fadeOut')
+            }, 3000)
+        })
+
     })
 })
