925a4949b737494f5a311acab4e439c6b8bc861a
Michael Ashefor <thatsureboy@gmail.com>
2019-07-12 12:48:27 +0100

    Add spinner on form submitting

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -13,12 +13,16 @@ window.addEventListener('load', function () {
     let submitting = document.getElementById('contactform')
     submitting.addEventListener('submit', function () {
         submitted = true;
+        document.getElementById('submitbutton').setAttribute('disabled', "")
+        document.getElementById('sendingmsg').style.display = 'inline-block'
         let hiddenframe = document.getElementById('hidden_iframe')
         hiddenframe.addEventListener('load', function () {
             if (submitted) {
                 document.getElementById('msg_success').style.display = 'block'
                 document.getElementById('msg_success').classList.add('fadeIn')
-                document.getElementById('contactform').reset()
+                document.getElementById('contactform').reset();
+                document.getElementById('submitbutton').removeAttribute('disabled')
+                document.getElementById('sendingmsg').style.display = 'none'
             }
             setTimeout(function(){
                 document.getElementById('msg_success').classList.add('fadeOut')
